f513d2c2c914c89d23607667258808103a580834
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-02-17 00:47:21 -0500

    Remove UseThroughBlock

--- src/main/java/dev/creoii/greatbigworld/GreatBigWorld.java
+++ src/main/java/dev/creoii/greatbigworld/GreatBigWorld.java
@@ -1,13 +1,7 @@
 package dev.creoii.greatbigworld;
 
-import dev.creoii.greatbigworld.item.UseThroughBlock;
 import dev.creoii.greatbigworld.worldgen.GBWPlacementModifiers;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.entity.Entity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,25 +11,6 @@ public class GreatBigWorld implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        PayloadTypeRegistry.playC2S().register(UseThroughBlock.AttackThroughBlock.PACKET_ID, UseThroughBlock.AttackThroughBlock.PACKET_CODEC);
-
         GBWPlacementModifiers.register();
-
-        ServerPlayNetworking.registerGlobalReceiver(UseThroughBlock.AttackThroughBlock.PACKET_ID, (payload, context) -> {
-            int entityId = payload.entityId();
-            context.server().execute(() -> {
-                ServerPlayerEntity serverPlayer = context.player();
-                if (serverPlayer.getServer() != null) {
-                    ItemStack stack = serverPlayer.getStackInHand(serverPlayer.getActiveHand());
-                    if (stack.getItem() instanceof UseThroughBlock useThroughBlock) {
-                        Entity entity = serverPlayer.getWorld().getEntityById(entityId);
-                        if (entity != null && useThroughBlock.canAttackThroughBlock(serverPlayer, stack, entity)) {
-                            serverPlayer.attack(entity);
-                            useThroughBlock.onAttackThroughBlock(serverPlayer, stack, entity);
-                        }
-                    }
-                }
-            });
-        });
     }
 }
