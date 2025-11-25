0b6634e6128501b56bd18825a4bd8433603ceb20
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-11-25 01:52:56 -0500

    Update GBWCoreMixinPlugin.java

--- src/main/java/dev/creoii/greatbigworld/GBWCoreMixinPlugin.java
+++ src/main/java/dev/creoii/greatbigworld/GBWCoreMixinPlugin.java
@@ -24,7 +24,7 @@ public class GBWCoreMixinPlugin implements IMixinConfigPlugin {
         if ("dev.creoii.greatbigworld.mixin.client.compat.ChunkBuilderMeshingTaskMixin".equals(mixinClassName)) {
             return FabricLoader.getInstance().isModLoaded("sodium");
         }
-        if ("dev.creoii.creoapi.mixin.collision.compat.LithiumEntityCollisionsMixin".equals(mixinClassName)) {
+        if ("dev.creoii.greatbigworld.mixin.compat.LithiumEntityCollisionsMixin".equals(mixinClassName)) {
             return FabricLoader.getInstance().isModLoaded("lithium");
         }
         return true;
