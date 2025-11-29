e26975d6ff999a74d88f8ec4e187529eb9379437
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-11-28 19:00:19 -0500

    Improve weather heightmap impl

--- src/main/java/dev/creoii/greatbigworld/mixin/client/WeatherRenderingMixin.java
+++ src/main/java/dev/creoii/greatbigworld/mixin/client/WeatherRenderingMixin.java
@@ -1,16 +1,18 @@
 package dev.creoii.greatbigworld.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.creoii.greatbigworld.util.GBWHeightmapTypes;
 import net.minecraft.client.render.WeatherRendering;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(WeatherRendering.class)
 public class WeatherRenderingMixin {
-    @Redirect(method = "buildPrecipitationPieces", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getTopY(Lnet/minecraft/world/Heightmap$Type;II)I"))
-    private int creo$applyWeatherRenderIgnores(World instance, Heightmap.Type heightmap, int x, int z) {
-        return instance.getTopY(Heightmap.Type.valueOf("WEATHER"), x, z);
+    @WrapOperation(method = "buildPrecipitationPieces", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getTopY(Lnet/minecraft/world/Heightmap$Type;II)I"))
+    private int creo$applyWeatherRenderIgnores(World instance, Heightmap.Type heightmap, int x, int z, Operation<Integer> original) {
+        return original.call(instance, GBWHeightmapTypes.WEATHER, x, z);
     }
 }
---
+++ src/main/java/dev/creoii/greatbigworld/util/GBWHeightmapTypes.java
@@ -0,0 +1,7 @@
+package dev.creoii.greatbigworld.util;
+
+import net.minecraft.world.Heightmap;
+
+public final class GBWHeightmapTypes {
+    public static final Heightmap.Type WEATHER = Heightmap.Type.valueOf("WEATHER");
+}
