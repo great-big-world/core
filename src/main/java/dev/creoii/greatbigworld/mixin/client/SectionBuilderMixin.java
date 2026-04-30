975e851af7741284fe01a0c450155cc7902cd7bd
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-04-30 01:11:58 -0400

    Fix snow overlays when Sodium not present

--- src/main/java/dev/creoii/greatbigworld/mixin/client/SectionBuilderMixin.java
+++ src/main/java/dev/creoii/greatbigworld/mixin/client/SectionBuilderMixin.java
@@ -5,6 +5,9 @@ import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexSorting;
 import dev.creoii.greatbigworld.block.OverlayState;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
+import net.minecraft.world.level.block.RenderShape;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,6 +16,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
+import java.util.Map;
+
 import net.minecraft.client.renderer.SectionBufferBuilderPack;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.block.model.BlockModelPart;
@@ -25,17 +30,29 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 
 @Mixin(SectionCompiler.class)
-public class SectionBuilderMixin {
+public abstract class SectionBuilderMixin {
     @Shadow @Final private BlockRenderDispatcher blockRenderer;
 
-    @Inject(method = "compile", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/BlockRenderDispatcher;renderBatched(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/BlockAndTintGetter;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLjava/util/List;)V"))
-    private void gbw$renderBlockOverlays(SectionPos sectionPos, RenderSectionRegion renderRegion, VertexSorting vertexSorter, SectionBufferBuilderPack allocatorStorage, CallbackInfoReturnable<SectionCompiler.Results> cir, @Local PoseStack matrixStack, @Local List<BlockModelPart> list, @Local RandomSource random, @Local(ordinal = 2) BlockPos blockPos3, @Local BufferBuilder bufferBuilder, @Local BlockState blockState) {
-        if (blockState.getBlock() instanceof OverlayState overlayState) {
+    @Shadow
+    protected abstract BufferBuilder getOrBeginLayer(Map<ChunkSectionLayer, BufferBuilder> map, SectionBufferBuilderPack sectionBufferBuilderPack, ChunkSectionLayer chunkSectionLayer);
+
+    @Inject(method = "compile", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isSolidRender()Z"))
+    private void gbw$renderBlockOverlays(SectionPos sectionPos, RenderSectionRegion renderRegion, VertexSorting vertexSorter, SectionBufferBuilderPack sectionBufferBuilderPack, CallbackInfoReturnable<SectionCompiler.Results> cir, @Local PoseStack poseStack, @Local List<BlockModelPart> list, @Local RandomSource random, @Local(ordinal = 2) BlockPos blockPos3, @Local BlockState blockState, @Local Map<ChunkSectionLayer, BufferBuilder> map) {
+        if (blockState.getBlock() instanceof OverlayState overlayState && blockState.getRenderShape() == RenderShape.MODEL) {
             BlockState overlay = overlayState.gbw$getOverlayState(blockState, blockPos3, random);
             if (overlay.getBlock() != Blocks.AIR) {
-                blockRenderer.getBlockModel(blockState).collectParts(random, list);
+                ChunkSectionLayer chunkSectionLayer = ItemBlockRenderTypes.getChunkRenderType(overlay);
+                BufferBuilder bufferBuilder = getOrBeginLayer(map, sectionBufferBuilderPack, chunkSectionLayer);
+                random.setSeed(overlay.getSeed(blockPos3));
+
+                blockRenderer.getBlockModel(overlay).collectParts(random, list);
+
+                poseStack.pushPose();
+                poseStack.translate((float)SectionPos.sectionRelative(blockPos3.getX()), (float)SectionPos.sectionRelative(blockPos3.getY()), (float)SectionPos.sectionRelative(blockPos3.getZ()));
+                blockRenderer.renderBatched(overlay, blockPos3, renderRegion, poseStack, bufferBuilder, true, list);
+                poseStack.popPose();
+
                 list.clear();
-                blockRenderer.renderBatched(overlay, blockPos3, renderRegion, matrixStack, bufferBuilder, true, list);
             }
         }
     }
